519c24b0a5d7975d44505e77ee01fd1731846f60
Bogdan <bogdandadayan@gmail.com>
2021-08-07 14:47:31 +0300

    upd

--- XamarinIOS/XamarinIOS/ViewController.cs
+++ XamarinIOS/XamarinIOS/ViewController.cs
@@ -1,5 +1,9 @@
 ﻿using Foundation;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using UIKit;
 
 namespace XamarinIOS
@@ -8,15 +12,47 @@ namespace XamarinIOS
     {
         public ViewController (IntPtr handle) : base (handle)
         {
+
         }
+
+        private static readonly HttpClient client = new HttpClient();
+
+        private readonly string helloUrl = "http://192.168.0.188:5000/api/values/";
+
 
         public override void ViewDidLoad ()
         {
+
+
+
             base.ViewDidLoad ();
 
-            btnPress.TouchUpInside += (object sender, EventArgs e) =>
+            btnPress.TouchUpInside += async (object sender, EventArgs e) =>
             {
-                var alert = UIAlertController.Create("Not supported", "Scheme 'tel:' is not supported on this device", UIAlertControllerStyle.Alert);
+                Dictionary<string, string> dict = new Dictionary<string, string>()
+            {
+                { "s", "Vasya"}
+            };
+
+                FormUrlEncodedContent form = new FormUrlEncodedContent(dict);
+
+                var values = new Dictionary<string, string>{
+              { "productId", "1" },
+              { "productKey", "Abc6666" },
+              { "userName", "OPPO" },
+            };
+
+                var json = JsonConvert.SerializeObject(values, Formatting.Indented);
+
+                var stringContent = new StringContent(json);
+
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.PostAsync(helloUrl, stringContent);
+
+                string result = await response.Content.ReadAsStringAsync();
+
+                var alert = UIAlertController.Create("Hello ", result, UIAlertControllerStyle.Alert);
                 alert.AddAction(UIAlertAction.Create("Ok", UIAlertActionStyle.Default, null));
                 PresentViewController(alert, true, null);
             };
